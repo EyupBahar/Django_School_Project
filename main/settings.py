d6791c7c46b0efa847df73f540dc485ec7088597
7204-OMER <Eyupbhr24@gmail.com>
2021-08-28 22:30:11 +0300

    add settings

--- main/settings.py
+++ main/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'temmplates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
